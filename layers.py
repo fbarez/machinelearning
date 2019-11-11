3b74bca1781cf5ef012c4ff5dbf2dddd347c9ba4
fazlbarez <fazlbarez93@gmail.com>
2019-11-11 12:45:29 +0000

    fixing nn

--- layers.py
+++ layers.py
@@ -39,3 +39,23 @@ class Layer(object):
             (batch_size, input_dim).
         """
         return grads_wrt_outputs * outputs * (1. - outputs)
+
+class network(object):
+
+    def update_mini_batch(self, mini_batch, eta):
+        """Update the network's weights and biases by applying
+        gradient descent using backpropagation to a single mini batch.
+        The "mini_batch" is a list of tuples "(x, y)", and "eta"
+        is the learning rate."""
+
+        nable_b = [np.zeros(b.shape) for b in self.biases]
+        nable_w = [np.zeros(w.shaoe) for w in self.weights]
+
+        for x,y in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.bprop(x,y)
+                        nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        self.weights = [w-(eta/len(mini_batch))*nw
+                        for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b-(eta/len(mini_batch))*nb
+                       for b, nb in zip(self.biases, nabla_b)]
